1ded4e90ec87aa8c68602a7a534604deb362c50f
yuvalingo <106463468+yuvalingo@users.noreply.github.com>
2024-12-30 19:03:34 +0200

    minor

--- benches/bench.rs
+++ benches/bench.rs
@@ -10,10 +10,16 @@ fn benchmark_sqrs(c: &mut Criterion) {
         rand::random::<u64>(),
         rand::random::<u64>(),
     ];
+    let input2: [u64; 4] = [
+        rand::random::<u64>(),
+        rand::random::<u64>(),
+        rand::random::<u64>(),
+        rand::random::<u64>(),
+    ];
     c.bench_function("sqr_cios_ord_unr", |b| b.iter(|| sqr_cios_ord_unr(black_box(input))));
     c.bench_function("sqr_cios_opt_unr_2", |b| b.iter(|| sqr_cios_opt_unr_2(black_box(input))));
     c.bench_function("sqr_cios_opt_unr_1", |b| b.iter(|| sqr_cios_opt_unr_1(black_box(input))));
-    c.bench_function("mul_cios_opt_unr_1", |b| b.iter(|| mul_cios_opt_unr_1(black_box(input), black_box(input))));
+    c.bench_function("mul_cios_opt_unr_1", |b| b.iter(|| mul_cios_opt_unr_1(black_box(input), black_box(input2))));
 }
 
 criterion_group!(benches, benchmark_sqrs);
